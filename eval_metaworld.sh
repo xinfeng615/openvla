dd4847e981fc10fae7b491c3e656b6062a9c57c8
xinfeng615 <115866550+xinfeng615@users.noreply.github.com>
2026-03-27 10:49:02 +0800

    Update eval_metaworld.sh

--- eval_metaworld.sh
+++ eval_metaworld.sh
@@ -1,8 +1,25 @@
+
+#!/bin/bash
+
 export CUDA_VISIBLE_DEVICES=0
 export MUJOCO_GL=egl
 
 python experiments/robot/metaworld/run_metaworld_eval.py \
   --model_family openvla \
-  --pretrained_checkpoint /path/to/checkpoint \
+  --pretrained_checkpoint /root/autodl-tmp/openvla/output/openvla-7b+metaworld_ml10_50e+b16+lr-0.0005+lora-r32+dropout-0.0--image_aug \
   --task_suite_name metaworld_ml10_50e \
-  --center_crop True
+  --center_crop True\
+  --use_wandb  True  \
+  --wandb_project "ml10-eval" \
+  --wandb_entity "1469512941-" 
+
+#export CUDA_VISIBLE_DEVICES=0
+#export MUJOCO_GL=egl
+
+# 使用 xvfb-run -a 解决无头服务器的 EGL 渲染崩溃问题
+
+#xvfb-run -a python experiments/robot/metaworld/run_metaworld_eval.py \
+  #--model_family openvla \
+  #--pretrained_checkpoint /root/autodl-tmp/openvla/openvla-7b \
+  #--task_suite_name metaworld_ml10_50e \
+  #--center_crop False
